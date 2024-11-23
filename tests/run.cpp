bb12583fdffc39716863462fd12c206c3b82cda0
davi.melonio <davi.melonio@eliis.fr>
2024-11-23 00:00:32 -0300

    added oop

---
+++ tests/run.cpp
@@ -0,0 +1,24 @@
+#include <cstring>
+#include "../include/SeisMath.hpp"
+
+int main (int argc, char *argv[]) {
+    uint16_t Nt = 101; 
+    float dt = 0.01;
+    float fmax = 10;
+    vec1d<float> freqs = {10.0f, 15.0f, 40.0f, 45.0f};
+
+    seismath::Wavelets waveletGenerator(Nt, dt, fmax, freqs);
+
+    vec1d<float> ricker = waveletGenerator.generateRicker();
+
+    for (size_t i = 1; i < argc; ++i) {
+
+        if (strcmp(argv[i], "-p") == 0) {
+            seismath::printPlot1D(ricker);
+        }
+    }
+
+    seismath::print1D(ricker);
+
+    return 0;
+}
